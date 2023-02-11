52f30a269d05145fd07327b9a95ddbdd1e4f63b4
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-02-11 12:46:16 +0300

    add query param '/query'

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask
+from flask import Flask, request, redirect
 
 import ws
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+@app.route('/<query>', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+def index(query=''):
+    if request.method == 'POST':
+        return redirect('/' + request.form['query'])
     with open('design.html', 'r', encoding='utf-8') as html_stream:
         html = html_stream.read()
 
-    weather = ws.get_weather()
+    weather = ws.get_weather(query)
     for replace in weather:
         html = html.replace(f'{{{{ {replace} }}}}', str(weather[replace]))
     return html
